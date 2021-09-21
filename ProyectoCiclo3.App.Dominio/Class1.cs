2a487d36b28b760a485c77e5d8eeffffed6eff51
Santapigo <stpinedag@gmail.com>
2021-09-20 19:41:54 -0500

    first commit

---
+++ ProyectoCiclo3.App.Dominio/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProyectoCiclo3.App.Dominio
+{
+    public class Class1
+    {
+        public int id {get; set;}
+        public string marca {get; set;} 
+        public int modelo {get; set;} 
+        public int kilometraje {get; set;} 
+        public int numero_asientos {get; set;} 
+        public string placa {get; set;} 
+    }
+}
