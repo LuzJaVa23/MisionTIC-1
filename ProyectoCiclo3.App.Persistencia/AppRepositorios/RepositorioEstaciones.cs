4840e3d65edc500178ff0e7318842ee8fc3e0b93
Santapigo <90393292+Santapigo@users.noreply.github.com>
2021-09-23 07:57:09 -0500

    commit

---
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioEstaciones.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using ProyectoCiclo3.App.Dominio;
+using System.Linq;
+using System;
+ 
+namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
+{
+    public class RepositorioEstaciones
+    {
+        List<Estaciones> estaciones;
+ 
+    public RepositorioEstaciones()
+        {
+            estaciones= new List<Estaciones>()
+            {
+                new Estaciones{id=1,nombre="Norte",direccion= "Avenida Norte calle 100",coord_x= 1,coord_y= 2,Tipo= "A"},
+                new Estaciones{id=2,nombre="Caracas",direccion= "Avenida Caracas calle 72",coord_x= 1,coord_y= 2,Tipo= "A"},
+                new Estaciones{id=3,nombre="Cll 80",direccion= "Portal de la 80",coord_x= 1,coord_y= 2,Tipo= "A"}
+             };
+        }
+ 
+        public IEnumerable<Estaciones> GetAll()
+        {
+            return estaciones;
+        }
+ 
+        public Estaciones GetBusWithId(int id){
+            return estaciones.SingleOrDefault(b => b.id == id);
+        }
+    }
+}
